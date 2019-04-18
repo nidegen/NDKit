6b01c25c4eb395cb8b73cc708828e6f783b28ce7
Nicolas Degen <nicolas@kapanu.com>
2019-04-18 17:05:55 +0200

    extend TimeInterval with static units

---
+++ Source/TimeInterval+Units.swift
@@ -0,0 +1,17 @@
+//
+//  TimeInterval+Units.swift
+//  NDKit
+//
+//  Created by Nicolas Degen on 18.04.19.
+//  Copyright © 2019 Nicolas Degen. All rights reserved.
+//
+
+import Foundation
+
+public extension TimeInterval {
+  static var minute: TimeInterval = 60
+  static var hour: TimeInterval = 60 * 60
+  static var day: TimeInterval = 60 * 60 * 24
+  static var week: TimeInterval = 60 * 60 * 24 * 7
+  static var year: TimeInterval = 60 * 60 * 24 * 365
+}
