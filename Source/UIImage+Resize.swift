007b4a24128244c3d5ee6b6ee16905051a2f086d
Nicolas Degen <nicolas@kapanu.com>
2019-09-12 11:51:48 +0200

    add image resize

---
+++ Source/UIImage+Resize.swift
@@ -0,0 +1,24 @@
+//
+//  UIImage+Resize.swift
+//  NDKit
+//
+//  Created by Nicolas Degen on 02.09.19.
+//  Copyright © 2019 Nicolas Degen. All rights reserved.
+//
+
+import Foundation
+
+extension UIImage {
+  public func resized(withPercentage percentage: CGFloat) -> UIImage? {
+    let canvas = CGSize(width: size.width * percentage, height: size.height * percentage)
+    return UIGraphicsImageRenderer(size: canvas, format: imageRendererFormat).image {
+      _ in draw(in: CGRect(origin: .zero, size: canvas))
+    }
+  }
+  public func resized(toWidth width: CGFloat) -> UIImage? {
+    let canvas = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+    return UIGraphicsImageRenderer(size: canvas, format: imageRendererFormat).image {
+      _ in draw(in: CGRect(origin: .zero, size: canvas))
+    }
+  }
+}
