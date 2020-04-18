9137a8007eadfd3bf8e3eca949bd2c413e5c5030
Nicolas Degen <nicolas@kapanu.com>
2020-04-18 17:33:54 +0200

    Hex color added

---
+++ Sources/NDKit/Color+Hex.swift
@@ -0,0 +1,26 @@
+//
+//  Color+Hex.swift
+//  NDKit
+//
+//  Created by Nicolas Degen on 18.04.20.
+//  Copyright © 2020 Nicolas Degen. All rights reserved.
+//
+
+import SwiftUI
+
+public extension Color {
+  public init(hex: Int, alpha: Double = 1) {
+    let components = (
+      R: Double((hex >> 16) & 0xff) / 255,
+      G: Double((hex >> 08) & 0xff) / 255,
+      B: Double((hex >> 00) & 0xff) / 255
+    )
+    self.init(
+      .sRGB,
+      red: components.R,
+      green: components.G,
+      blue: components.B,
+      opacity: alpha
+    )
+  }
+}
