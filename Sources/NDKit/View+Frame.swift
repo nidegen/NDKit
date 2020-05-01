d18e5c31aaff503536684622686bea4a34e822df
Nicolas Degen <nicolas@kapanu.com>
2020-05-01 09:19:46 +0200

    add proportional frame modifier

--- Sources/NDKit/View+Frame.swift
+++ Sources/NDKit/View+Frame.swift
@@ -12,7 +12,26 @@ public extension View {
   func height(_ height: CGFloat) -> some View {
     self.frame(height: height)
   }
+  
   func width(_ width: CGFloat) -> some View {
     self.frame(width: width)
+  }
+  
+  func proportional(width: CGFloat, height: CGFloat) -> some View {
+    GeometryReader { geometry in
+      self.frame(width: geometry.size.width * width, height: geometry.size.height * height)
+    }
+  }
+  
+  func proportional(width: CGFloat) -> some View {
+    GeometryReader { geometry in
+      self.frame(width: geometry.size.width * width)
+    }
+  }
+  
+  func proportional(height: CGFloat) -> some View {
+    GeometryReader { geometry in
+      self.frame(height: geometry.size.height * height)
+    }
   }
 }
